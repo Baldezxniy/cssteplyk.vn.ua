0018f8e9cf19c89e1477a9b714a7d9a7fc41a233
tolik <tolik@nginx-web-server.vozhd.net.ua>
2024-06-10 21:18:31 +0300

    deploying to vozhd-net-server

--- app/news/[newsId]/page.tsx
+++ app/news/[newsId]/page.tsx
@@ -37,7 +37,7 @@ const options = {
       // render the EMBEDDED_ASSET as you need
       return (
         <div className='flex justify-center'>
-          <img
+          <Image
             src={`https://${node.data.target.fields.file.url}`}
             height={node.data.target.fields.file.details.image.height}
             width={node.data.target.fields.file.details.image.width}
